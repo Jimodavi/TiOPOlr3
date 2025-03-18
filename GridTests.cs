c2b80de668522398435d5c98df5da3c729fc9cae
Liza <shovel.f@list.ru>
2025-03-18 13:03:53 +0700

    Тест создания сетки

---
+++ GridTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lr3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr3.Tests
+{
+    [TestClass()]
+    public class GridTests
+    {
+        [TestMethod()]
+        public void GridCreationTest()
+        {
+            Grid actual = new Grid();
+            Assert.IsNotNull(actual, "Сбой создания сетки. Ожидается not null объект класса.");
+        }
+    }
+}
