e2946875c883b06b05ba340b802a2f8cf9661fce
Liza <shovel.f@list.ru>
2025-03-18 13:07:49 +0700

    Добавлен класс сетки. Тест создания сетки успешный

---
+++ Grid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr3
+{
+    public class Grid
+    {
+        public Grid()
+        {
+
+        } 
+    }
+}
