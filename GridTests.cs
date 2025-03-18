8454e36e2b0c633ec4d5c44f055825e350e435e3
Liza <shovel.f@list.ru>
2025-03-18 17:51:38 +0700

    Тест увеличения значения сосендней ячейки справа

--- GridTests.cs
+++ GridTests.cs
@@ -201,5 +201,26 @@ namespace lr3.Tests
                 throw new AssertFailedException("Сбой увеличения значения сосендней ячейки справа сверху вне границ. Ожидается остановка обращения к индексу вне границ сетки.");
             }
         }
+
+        [TestMethod()]
+        public void GridRightAdjacentIncTest()
+        {
+            int expected = 1;
+            int rows = 55;
+            int columns = 5;
+            Grid actual = new Grid(rows, columns);
+            int cell_row = 49;
+            int cell_column = 2;
+
+            try
+            {
+                actual.RightAdjacentInc(cell_row, cell_column);
+                Assert.AreEqual(expected, actual[cell_row, cell_column + 1]);
+            }
+            catch (Exception)
+            {
+                throw new AssertFailedException("Сбой увеличения значения сосендней ячейки справа сверху. Ожидается " + expected + ". Фактически " + actual[cell_row - 1, cell_column + 1] + " .");
+            }
+        }
     }
 }
