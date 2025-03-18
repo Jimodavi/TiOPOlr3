4ab73a79d3ada689a9b7f2594ff111b9fd63502b
Liza <shovel.f@list.ru>
2025-03-18 14:36:25 +0700

    Добавлена тривиальная реализация получения значения ячейки сетки

--- Grid.cs
+++ Grid.cs
@@ -10,15 +10,26 @@ namespace lr3
     {
         public readonly int rows;
         public readonly int columns;
+        public readonly int[,] cells;
 
         public Grid()
         {
 
         }
+
         public Grid(int rows, int columns)
         {
             this.rows = rows;
             this.columns = columns;
+            this.cells = new int[rows, columns];
+        }
+
+        public int this[int row, int column]
+        {
+            //todo реализовать метод get позднее до конца
+            get { return 9; }
+            //todo реализовать метод set позднее до конца
+            set { }
         }
     }
 }
