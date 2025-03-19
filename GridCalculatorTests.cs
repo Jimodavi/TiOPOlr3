18a56db1e9127f94a3b2a46391fc8cc20b5422a8
Liza <shovel.f@list.ru>
2025-03-19 18:29:25 +0700

    Тест создания GridCalculator

---
+++ GridCalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lr3.Tests
+{
+    [TestClass()]
+    class GridCalculatorTests
+    {
+        [TestMethod()]
+        public void GridCalculatorCreationTest()
+        {
+            int rows = 20;
+            int columns = 19;
+            GridCalculator actual = new GridCalculator(new Grid(rows, columns));
+            Assert.IsNotNull(actual, "Сбой создания объекста GridCalculator. Ожидается not null объект класса.");
+        }
+    }
+}
