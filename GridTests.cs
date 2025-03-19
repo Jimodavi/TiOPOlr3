00f11c86dde056e9cc40c5afe53b2ccd859af3f7
Liza <shovel.f@list.ru>
2025-03-19 17:58:23 +0700

    Исправлен тест увеличения значения сосендней ячейки снизу вне границ

--- GridTests.cs
+++ GridTests.cs
@@ -318,7 +318,7 @@ namespace lr3.Tests
             }
             catch (IndexOutOfRangeException)
             {
-                throw new AssertFailedException("Сбой увеличения значения сосендней ячейки снизу вне границ. Ожидается остановка обращения к индексу вне границ сетки.");
+                throw new AssertFailedException("Сбой увеличения значения соседней ячейки снизу вне границ. Ожидается остановка обращения к индексу вне границ сетки.");
             }
         }
 
