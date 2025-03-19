21f460ec90b5a823e41cc4a7cfeecd82fe2acb60
Liza <shovel.f@list.ru>
2025-03-19 17:55:42 +0700

    Исправлен тест увеличения значения сосендней ячейки справа снизу

--- GridTests.cs
+++ GridTests.cs
@@ -259,7 +259,7 @@ namespace lr3.Tests
             }
             catch (Exception)
             {
-                throw new AssertFailedException("Сбой увеличения значения сосендней ячейки справа снизу. Ожидается " + expected + ". Фактически " + actual[cell_row + 1, cell_column + 1] + " .");
+                throw new AssertFailedException("Сбой увеличения значения соседней ячейки справа снизу. Ожидается " + expected + ". Фактически " + actual[cell_row + 1, cell_column + 1] + " .");
             }
         }
 
